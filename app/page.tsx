5b676f93164a0ef65f8f42ff0f6c2f045af849e7
Saulo Christian <saullochristian@outlook.com>
2024-08-10 10:22:23 -0300

    ✨ feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,22 @@ import Header from "./_components/header"
 import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
     <div>
       {/* header */}
       <Header />
+
+      {/*TEXTO*/}
       <div className="p-5">
         <h2 className="text-xl font-bold">Olá, Saulo</h2>
         <p>Sábado, 10 de agosto</p>
 
+        {/*BUSCA*/}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Buscar" />
           <Button size="icon">
@@ -20,6 +26,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/*IMAGEM*/}
         <div className="relative mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
@@ -28,6 +35,34 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        {/*AGENDAMENTO*/}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Agendamentos
+        </h2>
+
+        <Card>
+          <CardContent className="flex justify-between p-0">
+            {/*ESQUERDA*/}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Vintage Barber</p>
+              </div>
+            </div>
+
+            {/*DIREITA*/}
+            <div className="flex flex-col items-center justify-center border-l-2 border-dotted px-9">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">22</p>
+              <p className="text-sm">18:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
