2c8b77a49bbab9ca86dcb1eb1bc3dd98336bbaa0
Saulo Christian <saullochristian@outlook.com>
2024-08-13 17:05:50 -0300

    🐞 fix: Image Avatar

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -15,7 +15,7 @@ const SidebarSheet = () => {
 
       <div className="flex items-center gap-3 border-b border-solid py-5">
         <Avatar>
-          <AvatarImage src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+          <AvatarImage src="https://github.com/dev-saulo.png" />
         </Avatar>
         <div>
           <p className="font-bold">Saulo Christian</p>
